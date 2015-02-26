e4156f77f73abc20b677570b351032986df8c406
douglasbolis <douglasbolislima@gmail.com>
2015-02-26 14:38:06 -0300

    Falta só um pouquinho (terminando o exercicio 3)

--- lista_3/ex1.py
+++ lista_3/ex1.py
@@ -20,18 +20,30 @@ def f_insereFilmeAtores(dicAcervo):
     #fim while
 #fim funcao
 
-def f_exibeAtores(dicAcervo, filmeSt, filmeNd):
-    s=0
+def f_exibeAtores(dicAcervo, filmeSt, filmeNd, cod):
+    if cod == 1:
+        print("Atores de %s e %s:" %(filmeSt, filmeNd))
+        for el in dicAcervo[filmeSt]:
+            print("\t-%s" %(el))
+        #fim for
+        for el in dicAcervo[filmeNd]:
+            print("\t-%s" %(el))
+        #fim for
+    else:
+        if cod == 2:
+           s=0
+        else:
+            s=0
+        #fim if
+    #fim id
 #fim funcao
 
-def f_imprimeDados(dic):
+def f_imprimeFilmes(dic):
+    print("----------------- Filmes -----------------")
     for chave, valor in dic.items():
         print("%s" %(chave))
-        for elemento in valor:
-            print("\t- %s" %(elemento))
-        #fim for
-        print("")
     #fim for
+    print("")
 #fim funcao
 
 def main():
@@ -40,14 +52,24 @@ def main():
         "Ironman": ["Robert Downey Jr", "Terrence Howard", "Jeff Bridges"],
         "The Avengers": ["Robert Downey Jr", "Mark Rufallo", "Chris Hemsworth", "Chris Evans", "Scarlett Johansson", "Jeremy Renner"]
         }
-    nomeFilme, nomeAtor = "", ""
+    nomeFilme, nomeAtor, filmeSt, filmeNd, cod = "", "", "", "", 5
 
     #nomeFilme = input("Informe o nome do Filme: ")
     #nomeAtor = input("Informe o nome de um Ator para o Filme %s: " %(nomeFilme))
     #f_adicionaFilmeAtor(dicAcervo, nomeFilme, nomeAtor)
     #f_insereFilmeAtores(dicAcervo)
 
-    f_imprimeDados(dicAcervo)
+    f_imprimeFilmes(dicAcervo)
+
+    print("Escolha dois filmes da lista acima")
+    filmeSt = input("Primeiro Filme: ")
+    filmeNd = input("Segundo Filme: ")
+
+    while cod!= 0:
+        cod=1
+        f_exibeAtores(dicAcervo, filmeSt, filmeNd, cod)
+        cod=0
+    #fim while
 #fim main
 
 if __name__ == "__main__":
