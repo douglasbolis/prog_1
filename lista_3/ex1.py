419e10a617cbb5a1758c37e8aec3f52ee7360ed5
douglasbolis <douglasbolislima@gmail.com>
2015-02-26 11:57:44 -0300

    Terminando exercicio 3

--- lista_3/ex1.py
+++ lista_3/ex1.py
@@ -1,6 +1,6 @@
 __author__ = 'douglas'
 
-def f_adicionaFilmeAtores(d, f, n):
+def f_adicionaFilmeAtor(d, f, n):
 
     if f in d:
         d[f].append(n)
@@ -8,3 +8,46 @@ def f_adicionaFilmeAtores(d, f, n):
         d[f] = [n]
     #fim if
 #fim funcao
+
+def f_insereFilmeAtores(dicAcervo):
+    nomeFilme, nomeAtor, lstAtor = "", "", []
+
+    nomeFilme = input("Digite o nome do Filme: ")
+    nomeAtor = input("Digite o nome de um Ator para o Filme: ")
+
+    while nomeAtor != "":
+        f_adicionaFilmeAtor(dicAcervo, nomeFilme, nomeAtor)
+        nomeAtor = input("Digite o nome de um Ator para o Filme: [String vazia encerra]: ")
+    #fim while
+#fim funcao
+
+def f_exibeAtores(dicAcervo, filmeSt, filmeNd):
+    s=0
+#fim funcao
+
+def f_imprimeDados(dic):
+    for chave, valor in dic.items():
+        print("%s" %(chave))
+        for elemento in valor:
+            print("\t- %s" %(elemento))
+    #fim for
+    print("")
+#fim funcao
+
+def main():
+    dicAcervo = {"Achorman 2: The Lengend Continues":["Will Ferrell", "Steve Carell", "Paul Rudd", "Adam McKay"]}
+    nomeFilme, nomeAtor = "", ""
+
+    #nomeFilme = input("Informe o nome do Filme: ")
+    #nomeAtor = input("Informe o nome de um Ator para o Filme %s: " %(nomeFilme))
+    #f_adicionaFilmeAtor(dicAcervo, nomeFilme, nomeAtor)
+    #f_insereFilmeAtores(dicAcervo)
+
+    f_imprimeDados(dicAcervo)
+
+    print(dicAcervo)
+#fim main
+
+if __name__ == "__main__":
+    main()
+    #fim if
